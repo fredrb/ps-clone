dfd7a05d1e0e3fbffc3e81da10cf8b6fbffb27f8
Frederico <fred.rbittencourt@gmail.com>
2016-09-23 08:41:51 -0300

    remove command for grep compatibility

--- bin/ps.rb
+++ bin/ps.rb
@@ -3,9 +3,9 @@ require_relative '../lib/FilesystemAdapter.rb'
 
 processes = ProcessFactory.new FilesystemAdapter
 
-format="%6s\t%-15s\t%-10s\t%-10s\t%-10s\t%-30s\n"
-printf(format, "PID", "NAME", "USER", "STATE", "MEMORY", "COMMAND")
-printf(format, "------", "---------------", "----------", "----------", "----------", "------------------------------")
+format="%6s\t%-15s\t%-10s\t%-10s\t%-10s\n"
+printf(format, "PID", "NAME", "USER", "STATE", "MEMORY")
+printf(format, "------", "---------------", "----------", "----------", "----------")
 processes.each_process { |p|
-  printf(format, p.pid, p.name, p.user, p.state, p.rss, p.command)
+  printf(format, p.pid, p.name, p.user, p.state, p.rss)
 }
