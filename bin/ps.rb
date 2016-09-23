521507f53de889c7ec456a5f56279ebc1b3faba9
Frederico <fred.rbittencourt@gmail.com>
2016-09-23 08:40:00 -0300

    change format

--- bin/ps.rb
+++ bin/ps.rb
@@ -3,9 +3,9 @@ require_relative '../lib/FilesystemAdapter.rb'
 
 processes = ProcessFactory.new FilesystemAdapter
 
-format="%6s\t%-15s\t%-15s\t%-10s\t%10s\t%-30s\n"
+format="%6s\t%-15s\t%-10s\t%-10s\t%-10s\t%-30s\n"
 printf(format, "PID", "NAME", "USER", "STATE", "MEMORY", "COMMAND")
-printf(format, "------", "---------------", "---------------", "----------", "-------------", "------------------------------")
+printf(format, "------", "---------------", "----------", "----------", "----------", "------------------------------")
 processes.each_process { |p|
   printf(format, p.pid, p.name, p.user, p.state, p.rss, p.command)
 }
